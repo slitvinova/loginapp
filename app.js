bbb581b4dcc242a5c359ff90cdde5487d049eb33
slitvinova <svetlana_litvinova@inbox.lv>
2017-04-21 14:50:25 +0300

    Changed folder structure to MEAN app structure, added some libraries, routes and views

--- app.js
+++ app.js
@@ -4,25 +4,89 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var mongo = require('mongodb');
+var localStrategy = require('passport-local').Strategy;
+var exphbs = require('express-handlebars');
 
-var index = require('./routes/index');
+// paroļu hešošana
+var bcrypt = require('bcryptjs');
+// flash messages
+var flash = require('connect-flash');
+// mongodb ORM
+var mongoose = require('mongoose');
+
+// savienojums ar datu bāzi
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+
+
+var routes = require('./routes/index');
 var users = require('./routes/users');
 
+// inicializēt express lietotni
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+// body-parser, cookie-parser pāreja (middleware)
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// statiska mape stiliem, jquery u.t.t.
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+// Express Session pāreja (middleware)
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// In this example, the formParam value is going to get morphed into form body format useful for printing.
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'), 
+      root    = namespace.shift(), 
+      formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
+
+// pievienot Flash
+app.use(flash());
+
+// globālie mainīgie Flash paziņojumiem
+app.use(function(req, res, next) {
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	res.locals.error = req.flash('error');
+	next();
+});
+
+// passport inicializācija
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
@@ -32,6 +96,7 @@ app.use(function(req, res, next) {
   next(err);
 });
 
+/*
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
@@ -42,5 +107,6 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+*/
 
 module.exports = app;
